58e5447789608976c5c5504a5df357363aadb57f
rsperberg <rsperberg@gmail.com>
2015-02-07 10:46:17 -0500

    use Array.forEach() for replacements

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -8,8 +8,8 @@
 
 
 var fs = require("fs");
-var doclistPath = '/Users/sperberx/dev/essential-javascript-links/';
-var doclistName = 'README';
+var doclistPath = '/Users/sperberx/dev/essential-javascript-links/misc/';
+var doclistName = 'README-short1';
 var doclistAddon = '-new';
 var doclistExtension = '.md';
 
@@ -34,7 +34,7 @@ var aposVe = /'ve\b/g;  // I've
 var oAposR = /O'R/g;  //  O'Reilly
 var spaceQuot = / "\b/g;  //  open quote (eg, precedes a 'word boundary')
 var quotSpace = /\b"/g;  //  close quote (eg, is preceded by a 'word boundary')
-var spaceDashSpace = / - /g;
+var spaceDashSpace = / - /g;  //  em dash
 //  not yet defined:  single quotes within double quotes
 // var spaceApos = / '/
 // var aposSpace = /' /
@@ -52,6 +52,21 @@ var spaceLdq = ' “';
 var rdqSpace = '”';
 var spaceEmDashSpace = " — ";
 
+var replacements = [
+    { searchFor: /'d\b/g, replaceWith: "’d"},    // I'd
+    { searchFor: /'ll\b/g, replaceWith: "’ll"},    //  you'll
+    { searchFor: /'m\b/g, replaceWith: "’m"},    //  I'm
+    { searchFor: /'re\b/g, replaceWith: "’re"},    //  you're
+    { searchFor: /'s\b/g, replaceWith: "’s"},    //  it's
+    { searchFor: /'t\b/g, replaceWith: "’t"},   //  don't
+    { searchFor: /'ve\b/g, replaceWith: "’ve"},   //  I've
+    { searchFor: /O'R/g, replaceWith: "O’R"},    //  O'Reilly
+    { searchFor: /",/g, replaceWith: ',”'},    // comma outside quote mark
+    { searchFor: /"\./g, replaceWith: '.”'},    // period outside quote mark (transpose only)
+    { searchFor: /"\b/g, replaceWith: '“'},    //  open quote (eg, precedes a 'word boundary')
+    { searchFor: /\b"/g, replaceWith: '”'},    //  close quote (eg, is preceded by a 'word boundary') needs to be set to follow punctuation as well
+    { searchFor: / - /g, replaceWith: " — "}    //  em dash
+];
 
 /*  store components of path  */
 var pathAndFile = doclistPath + doclistName + doclistExtension;   //   /Users/sperberx/dev/essential-javascript-links/README.md
@@ -62,25 +77,22 @@ var aFile = fs.readFile(pathAndFile, 'utf8', function (err,data) {
         return console.log(err);
     }
 
-    //  This seems really awkward to me, but it's working
+    //  for each object in the replacements array, go through the document and make the replacement
     function cleanUp(someFile) {
-        var removes = [aposD,  aposLl, aposM, aposRe, aposS, aposT, aposVe, oAposR, spaceQuot, quotSpace, spaceDashSpace];
-        var replaceWiths = [rsqD,  rsqLl, rsqM, rsqRe, rsqS, rsqT, rsqVe, OrsqR, spaceLdq, rdqSpace, spaceEmDashSpace];
-
-        for (var counter = 0; counter < removes.length; counter++) {
-            someFile = someFile.replace(removes[counter], replaceWiths[counter]);
-        }
+        replacements.forEach(function(replacement) {
+            someFile = someFile.replace(replacement.searchFor, replacement.replaceWith);
+        })
         return someFile;
    }
     var result = cleanUp(data);
 
-//    console.log(result);
+    console.log(result);
     console.log('got result back');
 
-    fs.writeFile(pathAndFileNew, result, function (err) {
-      if (err) throw err;
-      console.log('It\'s saved!');
-    });
+    // fs.writeFile(pathAndFileNew, result, function (err) {
+    //   if (err) throw err;
+    //   console.log('It\'s saved!');
+    // });
 
 });
 
